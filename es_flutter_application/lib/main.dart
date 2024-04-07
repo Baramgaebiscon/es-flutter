1313dc792e202e5b4268be6f10ae3c0e02cd9fa8
5innim <bjm22247@gmail.com>
2024-04-07 15:23:37 +0900

    feat: Link up login api

--- es_flutter_application/lib/main.dart
+++ es_flutter_application/lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+import 'package:es_flutter_application/next.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +16,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -21,143 +23,91 @@ class MyApp extends StatelessWidget {
         title: 'Flutter Demo Home Page',
         userIdController: TextEditingController(),
         passwordController: TextEditingController(),
-        ),
+      ),
     );
   }
 }
 
 // ignore: must_be_immutable
 class MyHomePage extends StatelessWidget {
-  MyHomePage({super.key,
-   required this.title,
-   required this.userIdController,
-   required this.passwordController
-   });
+  MyHomePage(
+      {super.key,
+      required this.title,
+      required this.userIdController,
+      required this.passwordController});
 
   String title;
   TextEditingController userIdController;
   TextEditingController passwordController;
-  
+
+  Future<http.Response> login(String id, String pw) async {
+    var url = Uri.http('localhost:8080', '/login');
+
+    var body = json.encode({'id': id, 'password': pw});
+
+    return await http.post(url,
+        headers: {"Content-Type": "application/json"}, body: body);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    width: 300,
-                    child: CupertinoTextField(
-                      controller: userIdController,
-                      placeholder: '아이디를 입력해주세요',
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
+        body: Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 300,
+                child: CupertinoTextField(
+                  controller: userIdController,
+                  placeholder: '아이디를 입력해주세요',
+                  textAlign: TextAlign.center,
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    width: 300,
-                    child: CupertinoTextField(
-                      controller: passwordController,
-                      placeholder: '비밀번호를 입력해주세요',
-                      textAlign: TextAlign.center,
-                      obscureText: true,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    width: 250,
-                    child: ElevatedButton(
-                      onPressed: () async {
-                        // final loginCheck = await login(
-                        //     userIdController.text, passwordController.text);
-
-                        // if (loginCheck == '-1') {
-                        //   showDialog(
-                        //     context: context,
-                        //     builder: (BuildContext context) {
-                        //       return AlertDialog(
-                        //         title: Text('알림'),
-                        //         content: Text('아이디 또는 비밀번호가 올바르지 않습니다.'),
-                        //         actions: [
-                        //           TextButton(
-                        //             child: Text('닫기'),
-                        //             onPressed: () {
-                        //               Navigator.of(context).pop();
-                        //             },
-                        //           ),
-                        //         ],
-                        //       );
-                        //     },
-                        //   );
-                        // } else {
-                        //   Navigator.push(
-                        //     context,
-                        //     MaterialPageRoute(
-                        //       builder: (context) => MyAppPage(),
-                        //     ),
-                        //   );
-                        // }
-                      },
-                      child: Text('로그인'),
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-        )
-    );
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 300,
+                child: CupertinoTextField(
+                  controller: passwordController,
+                  placeholder: '비밀번호를 입력해주세요',
+                  textAlign: TextAlign.center,
+                  obscureText: true,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: 250,
+                child: ElevatedButton(
+                  onPressed: () async {
+                    print('call login');
+
+                    final loginReponse = await login(
+                        userIdController.text, passwordController.text);
+
+                    if (loginReponse.statusCode == 200) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => NextPage(title: 'next page',),
+                        ),
+                      );
+                    }
+                  },
+                  child: Text('로그인'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-
-//     return Scaffold(
-//       appBar: AppBar(
-
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-
-//         child: Column(
-
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), 
-//     );
-//   }
-// }
